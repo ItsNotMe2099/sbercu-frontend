1061b5ceaa77cd48ae5c8b36c2d52e0f8ed593b4
Nikita Kirillov <wrongnmail@yandex.ru>
2020-10-06 14:45:53 +0300

    Show/hide password icon complete

--- components/ui/InputPassword/index.tsx
+++ components/ui/InputPassword/index.tsx
@@ -1,38 +1,51 @@
+import React from 'react'
 import styles from './index.module.scss'
 
 interface Props {
   placeholder?: string
   meta: any
-  isShown: boolean
   input
   label
   type
 }
 
-const isShown = () => {
-  var input = document.getElementById('password-input')
-  if(input.getAttribute('type') == 'password') {
-    return true
-  }
-  else {
-    return false
-  }
+interface State {
+  isShown: boolean
 }
 
-export default function InputPassword(props: Props) {
-  const { error, touched } = props.meta
-  const { input, label, type } = props
+class InputPassword extends React.Component<Props, State> {
+
+  state = {
+    isShown: false,
+  }
+
+  handleClick = () => {
+    if(this.state.isShown) {
+      this.setState({
+        isShown: false,
+      })
+    }
+    else {
+      this.setState({
+        isShown: true,
+      })
+    }
+  }
+
+  render() {
+  const { error, touched } = this.props.meta
+  const { input, label, type } = this.props
+
   return (
     <div className={styles.root}>
       <input
-        id='password-input'
         className={styles.input}
-        type={props.isShown ? 'text' : 'password'}
+        type={this.state.isShown ? 'text' : 'password'}
         placeholder={label}
         {...input}
       />
-      <a href="#" onClick={isShown}>
-        {props.isShown ? <img className={styles.icon} src={`img/field/shown.svg`} alt="" /> : <img className={styles.icon} src={`img/field/notShown.svg`} alt="" />}
+      <a href="#" onClick={this.handleClick}>
+        {this.state.isShown ? <img className={styles.icon} src={`img/field/shown.svg`} alt="" /> : <img className={styles.icon} src={`img/field/notShown.svg`} alt="" />}
       </a>
       {error &&
         touched && (
@@ -42,3 +55,6 @@ export default function InputPassword(props: Props) {
     </div>
   )
 }
+}
+
+export default InputPassword
