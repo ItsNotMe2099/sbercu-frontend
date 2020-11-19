d7bf53f480931020f25c7d377e9cc5dc00f2ecdb
DESKTOP-MHK75C2\CrazyOwl <wrongnmail@yandex.ru>
2020-11-19 17:34:53 +0300

    SBERCU - 56 dropdown modifications

--- components/dashboard/TagSelect/index.tsx
+++ components/dashboard/TagSelect/index.tsx
@@ -13,15 +13,17 @@ interface Props {
 export const TagSelect = (props: Props) => {
   const dispatch = useDispatch();
   const dropdownRef = useRef(null);
+  const choosedRef = useRef(null);
   const [isActive, setIsActive] = useDetectOutsideClick(dropdownRef, false);
-  const onClick = (e) => {
+  const [isChoosed, setIsChoosed] = useDetectOutsideClick(choosedRef, false)
+  const onClick = (e) => { 
     e.preventDefault()
     setIsActive(!isActive);
     //dispatch(fetchTag())
     console.log("Click");
   }
   //const options = useSelector((state: IRootState) => state.CategoryTagReducer.tags)
-  const options = [{label: 'Системное мышление и решение проблем', value: 1}, {label: 'Клиентоцентричность', value: 2}, {label: 'Развитие команд и сотрудничество', value: 3}, {label: 'Продукты и рынки', value: 4}]
+  const options = [{label: 'Системное мышление и решение проблем', value: 1}, {label: 'Клиентоцентричность', value: 2}, {label: 'Развитие команд и сотрудничество', value: 3}, {label: 'Продукты и рынки', value: 4}, {label: 'Клиентоцентричность', value: 5}, {label: 'Клиентоцентричность', value: 6}, {label: 'Клиентоцентричность', value: 7}, {label: 'Клиентоцентричность', value: 8}]
   const [value, setValue] = useState(options.find(item => item.value));
 
   const handleOptionClick = (e, item) => {
@@ -32,20 +34,16 @@ export const TagSelect = (props: Props) => {
   }
   const handleActiveOptionClick = (e) => {
     e.preventDefault();
-    setIsActive(false);
+    console.log("Click");
+    setIsChoosed(!isChoosed)
   }
   return (
     <div className={styles.root}>
         <a href="#" onClick={onClick} className={cx(styles.dropDownTrigger, { [styles.dropdownTriggerActive]: isActive })}>{props.categoryLabel}</a>
         <nav ref={dropdownRef} className={cx(styles.dropDown, { [styles.dropDownActive]: isActive })}>
-          <div className={styles.options}>
             {value &&
-            options.map(item => (
-            <div className={styles.dropdownItem}><a href="" onClick={handleActiveOptionClick}>
-              <span className={styles.dropdownItemLabel}>{item.label}</span></a>
-            </div>))
-            }
-        </div>
+            options.map(item => (<a ref={choosedRef} className={cx(styles.dropdownItem, { [styles.choosed]: isChoosed })} onClick={handleActiveOptionClick}>
+              <span className={styles.dropdownItemLabel}>{item.label}</span></a>))}
         </nav>
     </div>
   );
