5fb18ad9e1ee26e41ed7002081e2fb85274460ff
Andrey <a1081515@yandex.ru>
2020-12-02 15:56:36 +0300

    fix

--- components/layout/Layout/index.tsx
+++ components/layout/Layout/index.tsx
@@ -1,8 +1,9 @@
+import { ReactElement } from "react";
 import styles from './index.module.scss'
 import { useSelector, useDispatch } from 'react-redux'
 
 interface Props{
-
+    children?: any
 }
 
 export default function Layout(props: Props){
