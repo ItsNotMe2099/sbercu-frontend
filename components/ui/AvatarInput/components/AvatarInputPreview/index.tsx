374640694d58c0f76b646dacbff1dc6f2d4d8284
Andrey <a1081515@yandex.ru>
2021-02-08 16:50:01 +0300

    fix image upload

--- components/ui/AvatarInput/components/AvatarInputPreview/index.tsx
+++ components/ui/AvatarInput/components/AvatarInputPreview/index.tsx
@@ -34,7 +34,7 @@ const AvatarInputPreview: FunctionComponent<Props> = props => {
     if(!srcValue){
       return;
     }
-    return `${srcValue.indexOf('blob:') === 0 ? srcValue : (`${process.env.REACT_APP_API_URL || 'https://dev.sbercu.firelabs.ru'}/api/media/files/${srcValue}`)}`
+    return `${srcValue.indexOf('blob:') === 0 ? srcValue : (`${process.env.NEXT_PUBLIC_API_URL || 'https://dev.sbercu.firelabs.ru'}/api/media/files/${srcValue}`)}`
   }
   return (
         <div className={styles.root}>
